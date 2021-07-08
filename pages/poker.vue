63e50b2919abd9d2bbf49099f675c565a51a0423
Kellen Presta <kpresta04@gmail.com>
2021-07-08 11:41:43 +0100

    front end work

--- pages/poker.vue
+++ pages/poker.vue
@@ -1,11 +1,94 @@
 <template>
   <div class="pokerBoard">
-    poker
+    <div class="boardInfo">
+      <div class="cards"></div>
+    </div>
+    <div class="humanCards">
+      <div class="cards">
+        {{ (humanHand && humanHand.map(card => card.shortString)) || "" }}
+      </div>
+      <div class="controls">
+        <button class="button">Call</button>
+        <button class="button">Fold</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import {
+  actions,
+  createMachine,
+  assign,
+  interpret,
+  spawn,
+  send,
+  sendParent
+} from "xstate";
+
+import { createPokerMachine } from "~/utils/poker";
+import { imageDict } from "~/utils/imageDict";
+
+export default {
+  data() {
+    return {
+      pokerGame: null,
+      humanTurn: false,
+      poker: createPokerMachine(),
+      service: null
+    };
+  },
+  computed: {
+    humanHand: function() {
+      if (this.pokerGame && this.pokerGame.children[1]) {
+        return this.pokerGame.children[1]._state.context.hand || [];
+      } else {
+        return [];
+      }
+    }
+  },
+  mounted() {
+    // let poker;
+    // let service;
+
+    //
+    // // console.log(poker);
+    // poker = createPokerMachine();
+    this.service = interpret(this.poker)
+      .onTransition(state => {
+        // elApp.dataset.state = state.toStrings().join(' ');
+        // console.log(state);
+        this.pokerGame = state;
+      })
+      .start();
+
+    this.service.send({
+      type: "CHOOSE_PLAYER_NUMBER",
+      value: 7
+    });
+    this.service.state.context.players[0].send({
+      type: "HUMAN_CALL_SMALL_BLIND",
+      value: 5
+    });
+    // console.log(service);
+  }
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.humanCards {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+.boardInfo {
+  position: absolute;
+  bottom: 50%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+</style>
